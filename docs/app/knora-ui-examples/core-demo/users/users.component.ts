ec4366429fba7f555d8f4594e76121950e0c7f2a
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-06-05 09:20:51 +0200

    docs (demo app): build prod version of the app to use on gh-page

--- docs/app/knora-ui-examples/core-demo/users/users.component.ts
+++ docs/app/knora-ui-examples/core-demo/users/users.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {ApiServiceError, User, UsersService} from '@knora/core';
-import {Example} from '../../../app.interfaces';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceError, AuthenticationRequestPayload, User, UsersService } from '@knora/core';
+import { Example } from '../../../app.interfaces';
 
 @Component({
     selector: 'app-users',
@@ -8,7 +8,7 @@ import {Example} from '../../../app.interfaces';
     styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
+    
     exampleGetAllUsers: Example = {
         title: 'getAllUsers()',
         subtitle: 'returns a list of all users in Knora',
@@ -27,7 +27,14 @@ export class UsersComponent implements OnInit {
         </div>
             `,
             ts: `public allUsers: User[];
+            
+            // the services from @knora/core should be public,
+            // if you want to use the loading status in the html template
+            // --> usersService.loading = true | false
+            constructor(public usersService: UsersService) { }
+
             [...]
+            
             this.usersService.getAllUsers()
                 .subscribe(
                     (result: User[]) => {
@@ -41,16 +48,58 @@ export class UsersComponent implements OnInit {
         }
     };
 
+    exampleAuthenticate: Example = {
+        title: 'authenticate() / login(\'email\',\'password\') / logout()',
+        subtitle: 'returns if a user is logged-in or not',
+        name: 'authenticate',
+        code: {
+            html: ``,
+            ts: `
+            isLoggedIn: boolean = false;
+            errorMessage: ApiServiceError;
+
+            // the services from @knora/core should be public,
+            // if you want to use the loading status in the html template
+            // --> usersService.loading = true | false
+            constructor(public usersService: UsersService) { }
+
+            [...]
+            
+            this.usersService.authenticate()
+            .subscribe(
+                (result: any) => {
+                    // if result == true: a user is logged-in,
+                    // in case of an error (ApiServiceError), the current user is not authorized to do something
+                    this.isLoggedIn = result;
+                },
+                (error: ApiServiceError) => {
+                    this.isLoggedIn = false;
+                    this.errorMessage = error;
+                }
+            );`,
+            scss: ``
+        }
+    }
+
+    userSimData: AuthenticationRequestPayload = {
+        email: 'root@example.com',
+        password: 'test1234'
+    };
 
     allUsers: User[];
 
     user: User;
+
+    isLoggedIn: boolean = false;
+
+    errorMessage: ApiServiceError;
 
     constructor(public usersService: UsersService) {
     }
 
     ngOnInit() {
         this.getAllUsers();
+        this.simulateAuthentication();
     }
 
     getAllUsers() {
@@ -75,5 +124,39 @@ export class UsersComponent implements OnInit {
                     console.error(error);
                 }
             );
+    }
+
+    simulateAuthentication() {
+        this.usersService.authenticate()
+            .subscribe(
+                (result: any) => {
+                    // if result == true: a user is logged-in,
+                    // in case of an error (ApiServiceError), the current user is not authorized to do something
+                    this.isLoggedIn = result;
+                },
+                (error: ApiServiceError) => {
+                    this.isLoggedIn = false;
+                    this.errorMessage = error;
+                }
+            );
+    }
+
+    login(data: AuthenticationRequestPayload) {
+        this.usersService.login(data.email, data.password)
+            .subscribe(
+                (result: any) => {
+                    this.isLoggedIn = result;
+                    // console.log('simulateLogin: ', result);
+                },
+                (error: ApiServiceError) => {
+                    this.errorMessage = error;
+                    // console.error('simulateLogin: ', error);
+                }
+            );
+    }
+
+    logout() {
+        this.usersService.logout();
+        this.simulateAuthentication();
     }
 }
