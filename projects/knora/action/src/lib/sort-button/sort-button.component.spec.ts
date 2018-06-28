82fdf7e56668928e7664a280db72b90b907f4e55
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-06-28 16:38:11 +0200

    test (action module): fix broken tests

--- projects/knora/action/src/lib/sort-button/sort-button.component.spec.ts
+++ projects/knora/action/src/lib/sort-button/sort-button.component.spec.ts
@@ -1,25 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SortButtonComponent } from './sort-button.component';
+import {SortButtonComponent} from './sort-button.component';
+import {MatIconModule, MatMenuModule} from '@angular/material';
 
 describe('SortButtonComponent', () => {
-  let component: SortButtonComponent;
-  let fixture: ComponentFixture<SortButtonComponent>;
+    let component: SortButtonComponent;
+    let fixture: ComponentFixture<SortButtonComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SortButtonComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MatIconModule,
+                MatMenuModule
+            ],
+            declarations: [SortButtonComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SortButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SortButtonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
