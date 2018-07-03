bed2f955bedbdefbb313fdee6dd2c495ae4ecdae
André Kilchenmann <github@milchkannen.ch>
2018-07-03 09:39:39 +0200

    enhancement (app module): clean up

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,24 +1,24 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 // / start with main-intro
-import {MainIntroComponent} from './landing-page/main-intro/main-intro.component';
+import { MainIntroComponent } from './landing-page/main-intro/main-intro.component';
 // /modules demo
-import {ModuleIndexComponent} from './partials/module-index/module-index.component';
-import {DemoIntroComponent} from './landing-page/demo-intro/demo-intro.component';
+import { ModuleIndexComponent } from './partials/module-index/module-index.component';
+import { DemoIntroComponent } from './landing-page/demo-intro/demo-intro.component';
 // examples: demo components
-import {ProgressIndicatorComponent} from './knora-ui-examples/action-demo/progress-indicator/progress-indicator.component';
-import {CoreDemoComponent} from './knora-ui-examples/core-demo/core-demo.component';
-import {ProjectsComponent} from './knora-ui-examples/core-demo/projects/projects.component';
-import {UsersComponent} from './knora-ui-examples/core-demo/users/users.component';
-import {ListsComponent} from './knora-ui-examples/core-demo/lists/lists.component';
-import {GroupsComponent} from './knora-ui-examples/core-demo/groups/groups.component';
-import {TreeComponent} from './material/tree/tree.component';
-import {ResourceComponent} from './knora-ui-examples/core-demo/resource/resource.component';
+import { ProgressIndicatorComponent } from './knora-ui-examples/action-demo/progress-indicator/progress-indicator.component';
+import { CoreDemoComponent } from './knora-ui-examples/core-demo/core-demo.component';
+import { ProjectsComponent } from './knora-ui-examples/core-demo/projects/projects.component';
+import { UsersComponent } from './knora-ui-examples/core-demo/users/users.component';
+import { ListsComponent } from './knora-ui-examples/core-demo/lists/lists.component';
+import { GroupsComponent } from './knora-ui-examples/core-demo/groups/groups.component';
+import { TreeComponent } from './material/tree/tree.component';
+import { ResourceComponent } from './knora-ui-examples/core-demo/resource/resource.component';
 
-import {LoginComponent} from './knora-ui-examples/authentication-demo/login/login.component';
-import {ActionDemoComponent} from './knora-ui-examples/action-demo/action-demo.component';
-import {SortButtonComponent} from './knora-ui-examples/action-demo/sort-button/sort-button.component';
-import {AdminImageComponent} from './knora-ui-examples/action-demo/admin-image/admin-image.component';
+import { LoginComponent } from './knora-ui-examples/authentication-demo/login/login.component';
+import { ActionDemoComponent } from './knora-ui-examples/action-demo/action-demo.component';
+import { SortButtonComponent } from './knora-ui-examples/action-demo/sort-button/sort-button.component';
+import { AdminImageComponent } from './knora-ui-examples/action-demo/admin-image/admin-image.component';
 
 const appRoutes: Routes = [
     {
