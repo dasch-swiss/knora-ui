33bedb9457c0c65f512ef2e19fb9f69b2c2c08d4
Flavie Laurens <flavilo@hotmail.fr>
2018-07-05 16:56:45 +0200

    feature (search module): add select-ontology component in a subfolder extended-search

---
+++ projects/knora/search/src/lib/extended-search/select-ontology/select-ontology.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kui-select-ontology',
+  templateUrl: './select-ontology.component.html',
+  styleUrls: ['./select-ontology.component.scss']
+})
+export class SelectOntologyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
