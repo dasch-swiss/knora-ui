b0595f2c40c4fd5655bf6d3074e93be68bd9f0b7
Flavie Laurens <flavilo@hotmail.fr>
2018-07-06 12:05:57 +0200

    feature (search module): create a specific constants file for search module

---
+++ projects/knora/search/src/lib/search-constants.ts
@@ -0,0 +1,29 @@
+export class SearchConstants {
+
+    public static EqualsComparisonOperator = '=';
+    public static EqualsComparisonLabel = 'Equals';
+
+    public static NotEqualsComparisonOperator = '!=';
+    public static NotEqualsComparisonLabel = 'Not_Equals';
+
+    public static GreaterThanComparisonOperator = '>';
+    public static GreaterThanComparisonLabel = 'Greater_Than';
+
+    public static GreaterThanEqualsComparisonOperator = '>=';
+    public static GreaterThanEqualsComparisonLabel = 'Greater_Than_Equals';
+
+    public static LessThanComparisonOperator = '<';
+    public static LessThanComparisonLabel = 'Less_Than';
+
+    public static LessThanEqualsComparisonOperator = '<=';
+    public static LessThanQualsComparisonLabel = 'Less_Than_Equals';
+
+    public static ExistsComparisonOperator = 'E';
+    public static ExistsComparisonLabel = 'Exists';
+
+    public static LikeComparisonOperator = 'regex';
+    public static LikeComparisonLabel = 'Like';
+
+    public static MatchComparisonOperator = 'contains';
+    public static MatchComparisonLabel = 'Match';
+}
