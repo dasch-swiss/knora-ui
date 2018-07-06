66f6c9cd685b1899ca7e960cbbdd9ef2fc5390b1
Flavie Laurens <flavilo@hotmail.fr>
2018-07-06 12:01:18 +0200

    feature (search module): add select-ontology component

--- projects/knora/search/src/lib/extended-search/select-ontology/select-ontology.component.ts
+++ projects/knora/search/src/lib/extended-search/select-ontology/select-ontology.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { OntologyMetadata } from '@knora/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'kui-select-ontology',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectOntologyComponent implements OnInit {
 
-  constructor() { }
+  @Input() formGroup: FormGroup;
+
+  @Input() ontologies: Array<OntologyMetadata>;
+
+  @Output() ontologySelected = new EventEmitter<string>();
+
+  form: FormGroup;
+
+  constructor(@Inject(FormBuilder) private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    // build a form for the named graph selection
+    this.form = this.fb.group({
+      ontology: [null, Validators.required]
+    });
+
+    // emit Iri of the ontology when being selected
+    this.form.valueChanges.subscribe((data) => {
+      this.ontologySelected.emit(data.ontology);
+    });
+
+    // add form to the parent form group
+    this.formGroup.addControl('ontology', this.form);
+
   }
 
 }
