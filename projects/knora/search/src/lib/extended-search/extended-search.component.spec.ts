bc7c640db5ef06b3680a07aae08ebe46a041a939
Flavie Laurens <flavilo@hotmail.fr>
2018-07-06 12:03:20 +0200

    enhancement (search module): add template, spec and class content in extended-search component

--- projects/knora/search/src/lib/extended-search/extended-search.component.spec.ts
+++ projects/knora/search/src/lib/extended-search/extended-search.component.spec.ts
@@ -1,25 +1,70 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
 
 import { ExtendedSearchComponent } from './extended-search.component';
+import { ApiService, OntologyMetadata } from '@knora/core';
 
 describe('ExtendedSearchComponent', () => {
-  let component: ExtendedSearchComponent;
-  let fixture: ComponentFixture<ExtendedSearchComponent>;
+    let componentInstance: ExtendedSearchComponent;
+    let fixture: ComponentFixture<ExtendedSearchComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ExtendedSearchComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ExtendedSearchComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ExtendedSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(inject([ExtendedSearchComponent, MockBackend], (component: ExtendedSearchComponent, mockBackend) => {
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        // define different mock responses for different API calls
+        const responses = {};
+        responses['http://0.0.0.0:3333/v2/ontologies/metadata'] = new Response(new ResponseOptions({ body: require('../../../../model/test-data/ontologycache/ontology-metadata.json') }));
+        responses['http://0.0.0.0:3333/v2/ontologies/allentities/http%3A%2F%2F0.0.0.0%3A3333%2Fontology%2F0801%2Fbeol%2Fv2'] = new Response(new ResponseOptions({ body: require('../../../../model/test-data/ontologycache/beol-complex-onto.json') }));
+        responses['http://0.0.0.0:3333/v2/ontologies/allentities/http%3A%2F%2Fapi.knora.org%2Fontology%2Fknora-api%2Fv2'] = new Response(new ResponseOptions({ body: require('../../../../model/test-data/ontologycache/beol-complex-onto.json') }));
+
+        mockBackend.connections.subscribe(c => {
+            const response = responses[c.request.url];
+            c.mockRespond(response);
+        });
+
+        fixture = TestBed.createComponent(ExtendedSearchComponent);
+        componentInstance = fixture.componentInstance;
+        fixture.detectChanges();
+
+    })));
+
+    it('should create', () => {
+        expect(componentInstance).toBeTruthy();
+    });
+
+    it('should correctly initialized the ontologies\' metadata', () => {
+
+        const expectedOntoMetata =
+            [
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/0001/anything/v2', 'The anything ontology'),
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/0001/something/v2', 'The something ontology'),
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/00FF/images/v2', 'The images demo ontology'),
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/0801/beol/v2', 'The BEOL ontology'),
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/0802/biblio/v2', 'The Biblio ontology'),
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/0803/incunabula/v2', 'The incunabula ontology'),
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/0804/dokubib/v2', 'The dokubib ontology'),
+                new OntologyMetadata('http://0.0.0.0:3333/ontology/08AE/webern/v2', 'The Anton Webern project ontology'),
+                new OntologyMetadata('http://api.knora.org/ontology/knora-api/v2', 'The knora-api ontology in the complex schema')
+            ];
+
+
+        expect(componentInstance.ontologies).toEqual(expectedOntoMetata);
+
+
+    });
+
+    /*it('should get the classes and properties for a specific ontology', async(inject([ExtendedSearchComponent, MockBackend], (component: ExtendedSearchComponent, mockBackend) => {
+
+          // TODO: this involves an asynchronous function: find a way to check for the results
+
+          const resClassesAndProps = componentInstance.getResourceClassesAndPropertiesForOntology('http://0.0.0.0:3333/ontology/0801/beol/v2')
+
+      })));*/
 });
