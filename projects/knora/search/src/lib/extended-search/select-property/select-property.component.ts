7d858556c01f496a9c9891ef6df893ff6ccc3b0d
Flavie Laurens <flavilo@hotmail.fr>
2018-07-06 08:22:14 +0200

    feature (search module): create select-property component

---
+++ projects/knora/search/src/lib/extended-search/select-property/select-property.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kui-select-property',
+  templateUrl: './select-property.component.html',
+  styleUrls: ['./select-property.component.scss']
+})
+export class SelectPropertyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
