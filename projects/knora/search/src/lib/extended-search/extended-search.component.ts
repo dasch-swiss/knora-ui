24a37a57ab5c3e62a65aeda1f3333541d57725a3
Flavie Laurens <flavilo@hotmail.fr>
2018-07-06 08:21:36 +0200

    feature (search module): create extended-search component

---
+++ projects/knora/search/src/lib/extended-search/extended-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kui-extended-search',
+  templateUrl: './extended-search.component.html',
+  styleUrls: ['./extended-search.component.scss']
+})
+export class ExtendedSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
