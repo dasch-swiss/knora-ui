3d6d0b3ca064bfaaadee275731a10f86bdda4ee9
Flavie Laurens <flavilo@hotmail.fr>
2018-07-06 08:23:38 +0200

    bug (search module): reverse pipe import and implementation issue

--- projects/knora/core/src/lib/declarations/knora-api/pipes/reverse.pipe.ts
+++ projects/knora/core/src/lib/declarations/knora-api/pipes/reverse.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
     transform(value: any): any {
-        if (value) return value.slice().reverse();
+        if (value) {
+            return value.slice().reverse();
+        }
     }
 
 }
