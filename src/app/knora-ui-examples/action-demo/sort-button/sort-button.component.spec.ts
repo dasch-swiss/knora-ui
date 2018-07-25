de7b289251327a596b0eb22e2205171fa46743d4
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-07-25 15:44:02 +0200

    fix (action demo): fix test

--- src/app/knora-ui-examples/action-demo/sort-button/sort-button.component.spec.ts
+++ src/app/knora-ui-examples/action-demo/sort-button/sort-button.component.spec.ts
@@ -1,10 +1,11 @@
-import { MatIconModule, MatCardModule, MatDividerModule, MatTabsModule } from '@angular/material';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatCardModule, MatDividerModule, MatIconModule, MatTabsModule } from '@angular/material';
 import { KuiActionModule } from '@knora/action';
 import { SortButtonComponent } from './sort-button.component';
 import { ModuleSubHeaderComponent } from '../../../partials/module-sub-header/module-sub-header.component';
-import { ExampleViewerComponent } from 'docs/app/partials/example-viewer/example-viewer.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ExampleViewerComponent } from '../../../partials/example-viewer/example-viewer.component';
+
 
 describe('SortButtonComponent', () => {
     let component: SortButtonComponent;
