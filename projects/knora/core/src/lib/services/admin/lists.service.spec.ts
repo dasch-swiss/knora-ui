7c2535323bb79c4631fb3e79711c6b7d7d4c3685
flavie laurens <flavie.laurens@unibas.ch>
2018-09-04 11:59:34 +0200

    test (core module): fix tests for listsService

--- projects/knora/core/src/lib/services/admin/lists.service.spec.ts
+++ projects/knora/core/src/lib/services/admin/lists.service.spec.ts
@@ -1,21 +1,70 @@
-import { inject, TestBed } from '@angular/core/testing';
-
+import { async, inject, TestBed } from '@angular/core/testing';
 import { ListsService } from './lists.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { KuiCoreModule } from '../../core.module';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ApiService } from '../api.service';
+import { List } from '@knora/core/lib/declarations/knora-api/admin/lists/list';
+import { incunabulaProjectIri, listsResponse, listsTestData } from '../../test-data/admin/shared-test-data';
 
-describe('ListsService', () => {
+fdescribe('ListsService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let listService: ListsService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
+        HttpClientTestingModule,
         KuiCoreModule.forRoot({ name: '', api: 'http://0.0.0.0:3333', app: '', media: '' })
       ],
-      providers: [ListsService]
+      providers: [
+        ApiService,
+        ListsService
+      ]
     });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    listService = TestBed.get(ListsService);
   });
 
-  it('should be created', inject([ListsService], (service: ListsService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  fdescribe('#getList', () => {
+    const expectLists: List[] = listsTestData;
+    const expectedList = listsResponse;
+
+    fit('should be created', inject([ListsService], (service: ListsService) => {
+      expect(service).toBeTruthy();
+    }));
+
+    fit('should return all lists', async(inject(
+      [ListsService], (service) => {
+
+        expect(service).toBeDefined();
+
+        service.getLists(incunabulaProjectIri).subscribe((result) => {
+          expect(result.body).toEqual(expectLists);
+        });
+
+        // listService should have made one request to GET lists from expected URL
+        const req = httpTestingController.expectOne((request) => {
+          return request.url.match(service.url) && request.method === 'GET';
+        });
+
+    })));
+
+    /* fit('should return one list', async(inject([ListsService], (service) => {
+      expect(service).toBeDefined();
+      service.getList(iri).subscribe((result) => {
+        expect(result.body).toEqual(expectList);
+      });
+    }))); */
+
+  });
+
 });
