0b5c0ad2e88596898e634ada20b0db42c0123196
Tobias Schweizer <t.schweizer@unibas.ch>
2018-10-29 14:41:35 +0100

    test (search): select ontologies component is initialized correctly

--- projects/knora/search/src/lib/extended-search/select-ontology/select-ontology.component.spec.ts
+++ projects/knora/search/src/lib/extended-search/select-ontology/select-ontology.component.spec.ts
@@ -16,6 +16,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { KuiCoreConfig } from '../../../../../core/src/lib/declarations';
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { OntologyMetadata } from '../../../../../core/src/lib/services';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 fdescribe('SelectOntologyComponent', () => {
     let testHostComponent: TestHostComponent;
@@ -37,6 +38,7 @@ fdescribe('SelectOntologyComponent', () => {
                 MatCheckboxModule,
                 MatDatepickerModule,
                 MatAutocompleteModule,
+                BrowserAnimationsModule,
                 RouterTestingModule.withRoutes([])
             ],
             providers: [
@@ -63,8 +65,34 @@ fdescribe('SelectOntologyComponent', () => {
 
     it('should create', () => {
         // access the test host component's child
-        expect(testHostComponent.selectOntologies).toBeTruthy();
+        expect(testHostComponent.selectOntosComp).toBeTruthy();
     });
+
+    it('should make sure that th ontologies have been initialized correctly', () => {
+        // access the test host component's child
+        expect(testHostComponent.selectOntosComp).toBeTruthy();
+
+        const selectOntologyCompInstance: SelectOntologyComponent = testHostComponent.selectOntosComp;
+
+        const ontoMeta = [
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/0001/anything/v2', 'The anything ontology'),
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/0001/something/v2', 'The something ontology'),
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/00FF/images/v2', 'The images demo ontology'),
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/0801/beol/v2', 'The BEOL ontology'),
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/0802/biblio/v2', 'The Biblio ontology'),
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/0803/incunabula/v2', 'The incunabula ontology'),
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/0804/dokubib/v2', 'The dokubib ontology'),
+            new OntologyMetadata('http://0.0.0.0:3333/ontology/08AE/webern/v2', 'The Anton Webern project ontology'),
+            new OntologyMetadata('http://api.knora.org/ontology/knora-api/v2', 'The knora-api ontology in the complex schema')
+        ];
+
+        expect(selectOntologyCompInstance.ontologies).toEqual(ontoMeta);
+
+    });
+
+    
+
+
 
 });
 
@@ -74,21 +102,36 @@ fdescribe('SelectOntologyComponent', () => {
 @Component({
     selector: `host-component`,
     template: `
-        <kui-select-ontology #ontologies [formGroup]="form" [ontologies]="ontologies"></kui-select-ontology>`
+        <kui-select-ontology #ontosComp [formGroup]="form" [ontologies]="ontos"></kui-select-ontology>`
 })
 class TestHostComponent implements OnInit {
 
     form;
 
-    ontologies: Array<OntologyMetadata> = [];
+    ontos: Array<OntologyMetadata> = [];
 
-    @ViewChild('ontologies') selectOntologies: SelectOntologyComponent;
+    @ViewChild('ontosComp') selectOntosComp: SelectOntologyComponent;
+
+    private ontoMeta = [
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/0001/anything/v2', 'The anything ontology'),
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/0001/something/v2', 'The something ontology'),
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/00FF/images/v2', 'The images demo ontology'),
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/0801/beol/v2', 'The BEOL ontology'),
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/0802/biblio/v2', 'The Biblio ontology'),
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/0803/incunabula/v2', 'The incunabula ontology'),
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/0804/dokubib/v2', 'The dokubib ontology'),
+        new OntologyMetadata('http://0.0.0.0:3333/ontology/08AE/webern/v2', 'The Anton Webern project ontology'),
+        new OntologyMetadata('http://api.knora.org/ontology/knora-api/v2', 'The knora-api ontology in the complex schema')
+    ];
 
     constructor(@Inject(FormBuilder) private fb: FormBuilder) {
     }
 
     ngOnInit() {
         this.form = this.fb.group({});
+
+        this.ontos = this.ontoMeta;
+
     }
 
 }
