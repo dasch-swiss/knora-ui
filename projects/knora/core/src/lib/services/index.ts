ccfe157b1bb2feade99577daefc956dd4e6f8006
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2018-11-12 14:54:13 +0100

    fix (core): remove redundant export statements (#111)
    * fix (core): remove redundant export statements
    
    * refactor (core module): removed unused declarations

--- projects/knora/core/src/lib/services/index.ts
+++ projects/knora/core/src/lib/services/index.ts
@@ -16,18 +16,12 @@ export * from './admin/status-msg.service';
 /**
  * specific services for knora v2 api
  */
-export * from './v2/convert-jsonld';
-export * from './v2/incoming.service';
 export * from './v2/ontology.service';
 export * from './v2/ontology-cache.service';
 export * from './v2/resource.service';
 export * from './v2/search.service';
-export * from './v2/resource.service';
 export * from './v2/convert-jsonld';
-export * from './v2/ontology-cache.service';
-export * from './v2/ontology.service';
 export * from './v2/incoming.service';
-export * from './v2/search.service';
 export * from './v2/search-params.service';
 export * from './v2/grav-search.service';
 export * from './v2/store.service';
