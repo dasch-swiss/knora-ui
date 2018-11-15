953cc8fbbbaabc06e1de186257324ba0a05c2b6c
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-11-15 17:07:00 +0100

    docs (action): more on module docs with compodoc

--- projects/knora/action/src/lib/progress-indicator/progress-indicator.component.ts
+++ projects/knora/action/src/lib/progress-indicator/progress-indicator.component.ts
@@ -1,5 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+/**
+ *
+ *
+ * @example
+ * // default progress indicator
+ * <kui-progress-indicator color="#ff00aa"></kui-progress-indicator>
+ *
+ * @example
+ * // submit progress indicator: html
+ * <mat-list>
+ *                 <mat-list-item *ngFor="let item of examples">
+ *                     <kui-progress-indicator mat-list-avatar [status]="item.status"></kui-progress-indicator>
+ *                     <p mat-line><strong><span>{{item.label}}</span></strong></p>
+ *                     <p mat-line>status: {{item.status}}</p>
+ *                 </mat-list-item>
+ * </mat-list>
+ *
+ * @example
+ * // instead of [color]: overwrite the progress indicator color in a global scss file in the app.
+ *
+ * .kui-progress-indicator {
+ *                .line > div {
+ *                     background-color: #ff0000 !important;
+ *                 }
+ * }
+ *
+ *
+ */
 @Component({
     selector: 'kui-progress-indicator',
     templateUrl: './progress-indicator.component.html',
@@ -8,17 +36,21 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProgressIndicatorComponent implements OnInit {
 
     /**
-     * TODO: add description
+     * status is a number and can be used when submitting form data:
      *
-     * status is a number
      * - not ready:    -1
      * - loading:       0
      * - done:          1
      *
      * - error:       400
      */
-
     @Input() status?: number;
+
+    /**
+     * With [color] you can customize the appearance of the loader.
+     * You can define the color with the hexadecimal value e.g. #00ff00.
+     *
+     */
     @Input() color?: string = 'primary';
 
     constructor() {
