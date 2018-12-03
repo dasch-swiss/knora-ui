d89a11363483386feef0bd82b58164a96ec148a2
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-12-03 10:18:53 +0100

    docs (script): shell script to update the documentation

---
+++ update_docs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# update_docs: As it says, it updates the docs
+#
+# 1) update the documentation data from JSDocs of the ts files
+# 2) build the ng-app and update the files in docs/ folder
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 0) some definitions and functions
+#
+set -e
+
+dir=`pwd`
+sep='---------------------------------'
+
+die ()
+{
+    echo >&2 "$@"
+    exit 1
+}
+
+usage ()
+{
+    echo "usage: <command>"
+    echo ${sep}
+#    echo "-i = InputPath"
+#    echo "-o = OutputPath"
+#    echo ${sep}
+}
+
+inPath='./projects/knora/'
+outPath='./src/data/documentation/'
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 1) update the documentation data from JSDocs of the ts files
+#
+
+# we should loop through the inPath to write the documentation data files as following:
+dox < projects/knora/action/src/lib/admin-image/admin-image.directive.ts > src/data/documentation/action/admin-image.json
+
+# read the files
+#for i in ${inPath}*; do
+#    if [ -d "$i" ]; then
+#        echo ${i}
+#    fi
+#done
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 2) build the ng-app and update the files in docs/ folder
+#
+ng build --prod=false
+rm -rf docs/*
+mv dist/knora-ui docs
