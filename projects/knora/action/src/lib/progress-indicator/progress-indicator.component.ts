475b274c0bd591a8e3fa197deee7b5dd334e0cd5
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-12-03 14:08:22 +0100

    docs (style): docuementation viewer styling

--- projects/knora/action/src/lib/progress-indicator/progress-indicator.component.ts
+++ projects/knora/action/src/lib/progress-indicator/progress-indicator.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 /**
- * The progress indicator can be used the show the status of loading something.
+ * The progress indicator can be used to show the status of loading something.
  * This can be the simple loader or in case of submitting data it can show the status (not ready, loading, done or error).
+ *
+ * selector: kui-progress-indicator
  */
 @Component({
     selector: 'kui-progress-indicator',
