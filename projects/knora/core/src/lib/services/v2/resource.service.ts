f30b37385f7913a098cc8b25d774683b51955d89
Tobias Schweizer <t.schweizer@unibas.ch>
2018-12-13 17:01:55 +0100

    Revert "refactor (core): handle JSONLD conversion in ResourceService"
    This reverts commit 2b83fc73c213aee278ae7c3eb2350f027d5f9779.

--- projects/knora/core/src/lib/services/v2/resource.service.ts
+++ projects/knora/core/src/lib/services/v2/resource.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { ApiService } from '../api.service';
-import { ApiServiceResult, ReadResourcesSequence } from '../../declarations';
-import { map, mergeMap } from 'rxjs/operators';
-import { ConvertJSONLD } from './convert-jsonld';
-
-declare let require: any; // http://stackoverflow.com/questions/34730010/angular2-5-minute-install-bug-require-is-not-defined
-const jsonld = require('jsonld');
+import { ApiServiceResult } from '../../declarations';
 
 @Injectable({
     providedIn: 'root'
@@ -21,32 +16,9 @@ export class ResourceService extends ApiService {
      * @returns {Observable<any>}
      */
 
-    getResource(iri): Observable<ReadResourcesSequence> {
+    getResource(iri): Observable<ApiServiceResult> {
         // console.log('IRI from resource service: ', iri);
-        const res: Observable<any> = this.httpGet('/v2/resources/' + encodeURIComponent(iri));
-
-        return res.pipe(
-            mergeMap(
-                // this would return an Observable of a PromiseObservable -> combine them into one Observable
-                // http://reactivex.io/documentation/operators/flatmap.html
-                // http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-mergeMap
-                (resourceResponse: ApiServiceResult) => {
-                    const resPromises = jsonld.promises;
-                    // compact JSON-LD using an empty context: expands all Iris
-                    const resPromise = resPromises.compact(resourceResponse.body, {});
-
-                    // convert promise to Observable and return it
-                    // https://www.learnrxjs.io/operators/creation/frompromise.html
-                    return from(resPromise);
-                }
-            ),
-            map(
-                (resourceResponse: Object) => {
-                    const resSeq: ReadResourcesSequence = ConvertJSONLD.createReadResourcesSequenceFromJsonLD(resourceResponse);
-                    return resSeq;
-                }
-            )
-        );
+        return this.httpGet('/v2/resources/' + encodeURIComponent(iri));
     }
 
     /*
