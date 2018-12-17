4d57827f8ccf4154027611ce081028dac8ebeaa2
Flavie Laurens <flavilo@hotmail.fr>
2018-12-17 17:19:36 +0100

    docs (core-demo): add example for ontology service

--- src/app/knora-ui-examples/core-demo/ontology/ontology.component.ts
+++ src/app/knora-ui-examples/core-demo/ontology/ontology.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppDemo } from '../../../app.config';
+import { Example } from '../../../app.interfaces';
 
 @Component({
     selector: 'app-ontology',
@@ -9,6 +10,32 @@ import { AppDemo } from '../../../app.config';
 export class OntologyComponent implements OnInit {
 
     module = AppDemo.coreModule;
+
+    exampleOntology: Example = {
+        title: 'getOntologiesMetadata()',
+        subtitle: 'Requests the metadata about all existing ontologies from Knora\'s ontologies route.',
+        name: 'getOntologiesMetadata()',
+        code: {
+            html: ``,
+            ts: `
+getOntologiesMetadataFromKnora(): Observable<object> {
+
+    return this._ontologyService.getOntologiesMetadata().pipe(
+        mergeMap(
+            // this would return an Observable of a PromiseObservable
+            // -> combine them into one Observable
+            (ontRes: ApiServiceResult) => {
+                const ontPromises = jsonld.promises;
+                // compact JSON-LD using an empty context: expands all Iris
+                const ontPromise = ontPromises.compact(ontRes.body, {});
+                // convert promise to Observable and return it
+                return from(ontPromise);
+    }));
+}
+`,
+            scss: ``
+        }
+    };
 
     constructor() {
     }
