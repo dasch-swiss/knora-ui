13c71d4319b2e072fb06f718051a8040515cb143
Flavie Laurens <flavilo@hotmail.fr>
2018-12-17 17:38:32 +0100

    docs (core-demo): add example for ontology cache service

--- src/app/knora-ui-examples/core-demo/ontology-cache/ontology-cache.component.ts
+++ src/app/knora-ui-examples/core-demo/ontology-cache/ontology-cache.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppDemo } from '../../../app.config';
+import { Example } from '../../../app.interfaces';
 
 @Component({
     selector: 'app-ontology-cache',
@@ -9,6 +10,28 @@ import { AppDemo } from '../../../app.config';
 export class OntologyCacheComponent implements OnInit {
 
     module = AppDemo.coreModule;
+
+    exampleOntologyCache: Example = {
+        title: 'getResourceClassDefinitions(resourceClassIris)',
+        subtitle: 'Returns the definitions for the given resource class Iris.',
+        name: 'getResourceClassDefinitions',
+        code: {
+            html: ``,
+            ts: `
+// resource class Iris gotten from ConvertJsonLd.getResourceClassesFromJsonLD();
+resourceClassIris: string[];
+
+constructor(
+    private _cacheService: OntologyCacheService) {}
+
+this._cacheService.getResourceClassDefinitions(resourceClassIris).subscribe(
+    (resourceClassInfos: any) => {
+        // e.g. initialize ontology information and request incoming resources
+    }
+`,
+            scss: ``
+        }
+    };
 
     constructor() {
     }
