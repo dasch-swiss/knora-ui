bd5412a960ecf302e5637ba996c425bbee671c20
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-12-17 14:23:28 +0100

    enhancement (docs): gravSearchService and groupsService example

--- src/app/knora-ui-examples/core-demo/gravsearch/gravsearch.component.ts
+++ src/app/knora-ui-examples/core-demo/gravsearch/gravsearch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppDemo } from '../../../app.config';
+import { Example } from '../../../app.interfaces';
 
 @Component({
     selector: 'app-gravsearch',
@@ -9,6 +10,28 @@ import { AppDemo } from '../../../app.config';
 export class GravsearchComponent implements OnInit {
 
     module = AppDemo.coreModule;
+
+    exampleGravSearch: Example = {
+        title: 'getAllGroups()',
+        subtitle: 'returns a list of all (project specific) groups',
+        name: 'getAllGroups',
+        code: {
+            html: ``,
+            ts: `
+// iri of the resource class
+const resClass: string;
+
+// selected properties to filter by 
+const properties: PropertyWithValue[];
+
+const gravsearch: string = this._gravSearchService.createGravsearchQuery(properties, resClass, 0);
+                    
+// e.g. you can use the generated gravsearch query with the 
+// searchService and the method doExtendedSearch(gravsearch)
+`,
+            scss: ``
+        }
+    };
 
     constructor() {
     }
--- src/app/partials/example-viewer/example-viewer.component.ts
+++ src/app/partials/example-viewer/example-viewer.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Example } from '../../app.interfaces';
 
 declare var Prism;
@@ -8,7 +8,7 @@ declare var Prism;
     templateUrl: './example-viewer.component.html',
     styleUrls: ['./example-viewer.component.scss']
 })
-export class ExampleViewerComponent implements OnInit, AfterViewInit {
+export class ExampleViewerComponent implements OnInit {
 
     @Input() example: Example;
 
@@ -16,22 +16,19 @@ export class ExampleViewerComponent implements OnInit, AfterViewInit {
     typescript: string;
     style: string;
 
-    showCode: boolean = false;
+    showCode: boolean = true;
 
     constructor() {
     }
 
     ngOnInit() {
+        this.markup = Prism.highlight(this.example.code.html, Prism.languages.markup);
+        this.typescript = Prism.highlight(this.example.code.ts, Prism.languages.javascript);
+        this.style = Prism.highlight(this.example.code.scss, Prism.languages.scss);
     }
 
     toggleCode() {
         this.showCode = this.showCode !== true;
-    }
-
-    ngAfterViewInit() {
-        this.markup = Prism.highlight(this.example.code.html, Prism.languages.markup);
-        this.typescript = Prism.highlight(this.example.code.ts, Prism.languages.javascript);
-        this.style = Prism.highlight(this.example.code.scss, Prism.languages.scss);
     }
 
 }
