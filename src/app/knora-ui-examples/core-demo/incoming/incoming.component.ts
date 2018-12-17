77fc4d814ea567a1f18689e3dbfb9ccf3567da10
Flavie Laurens <flavilo@hotmail.fr>
2018-12-17 15:25:15 +0100

    docs (core-demo): add example demo for incoming service

--- src/app/knora-ui-examples/core-demo/incoming/incoming.component.ts
+++ src/app/knora-ui-examples/core-demo/incoming/incoming.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppDemo } from '../../../app.config';
+import { Example } from '../../../app.interfaces';
 
 @Component({
     selector: 'app-incoming',
@@ -9,6 +10,32 @@ import { AppDemo } from '../../../app.config';
 export class IncomingComponent implements OnInit {
 
     module = AppDemo.coreModule;
+
+    exampleIncoming: Example = {
+        title: 'getIncomingLinksForResource(resourceIri, offset)',
+        subtitle: 'Returns all incoming links for the given resource Iri.',
+        name: 'getIncomingLinksForResource',
+        code: {
+            html: ``,
+            ts: `
+resource: ReadResource;
+
+constructor(
+    private _incomingService: IncomingService,
+) {}
+
+private getIncomingLinks(offset: number, callback?: (numberOfResources: number) => void): void {
+    this._incomingService.getIncomingLinksForResource(this.resource.id, offset).subscribe(
+        (result: ApiServiceResult) => {
+            // e.g. you can then get resource class iris from the response and
+            // request ontology information about resource class Iris
+        }
+    );
+}
+`,
+            scss: ``
+        }
+    };
 
     constructor() {
     }
