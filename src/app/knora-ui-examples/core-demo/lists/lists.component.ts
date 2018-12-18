410086be3badf0410803e0a8f5935ed3b5b0b013
Flavie Laurens <flavilo@hotmail.fr>
2018-12-18 10:55:09 +0100

    docs (core-demo): improve example for lists service

--- src/app/knora-ui-examples/core-demo/lists/lists.component.ts
+++ src/app/knora-ui-examples/core-demo/lists/lists.component.ts
@@ -2,6 +2,7 @@ import { FlatTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceError, List, ListNodeInfo, ListsService } from '@knora/core';
 import { AppDemo } from '../../../app.config';
+import { Example } from '../../../app.interfaces';
 
 class ListNode {
     label: string;
@@ -23,6 +24,39 @@ export class ListsComponent implements OnInit {
     projectLists: ListNodeInfo[];
 
     treeControl: FlatTreeControl<ListNode>;
+
+    exampleGetLists: Example = {
+        title: 'getAllLists()',
+        subtitle: 'returns a list of all lists',
+        name: 'getAllLists',
+        code: {
+            html: `
+<div *ngFor="let item of projectLists">
+    <p>{{item.labels[0].value}}</p>
+    <p *ngIf="item.comments.length > 0">{{item.comments[0].value}}</p>
+</div>`,
+            ts: `
+projectIri: string;
+
+projectLists: ListNodeInfo[];
+
+constructor(public _listsService: ListsService) {}
+
+ngOnInit() {
+    this._listsService.getLists(this.projectIri)
+        .subscribe(
+            (result: ListNodeInfo[]) => {
+                this.projectLists = result;
+            },
+            (error: ApiServiceError) => {
+                console.error(error);
+            }
+        );
+
+}`,
+            scss: ``
+        }
+    };
 
     constructor(public listsService: ListsService) {
     }
