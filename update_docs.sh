e58fb951995c156bb78f9139c5cedac9c8847081
André Kilchenmann <a.kilchenmann@unibas.ch>
2018-12-20 10:55:47 +0100

    update documentation

--- update_docs.sh
+++ update_docs.sh
@@ -134,6 +134,6 @@ jsdoc2json "${special[@]}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 3) build the ng-app and update the files in docs/ folder
 #
-# git rm -r docs/*
-# ng build --prod=false --base-href /Knora-ui/ --build-optimizer --aot --output-path docs
-# cp docs/index.html docs/404.html
+git rm -r docs/*
+ng build --prod=false --base-href /Knora-ui/ --build-optimizer --aot --output-path docs
+cp docs/index.html docs/404.html
