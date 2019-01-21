f753390407dc687288f085700d578866942f1184
Flavie Laurens <flavilo@hotmail.fr>
2019-01-21 11:40:35 +0100

    enhancement (list-view): implement getResult method in a simple way - wip

--- projects/knora/viewer/src/lib/view/list-view/list-view.component.ts
+++ projects/knora/viewer/src/lib/view/list-view/list-view.component.ts
@@ -1,15 +1,157 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    ApiServiceError,
+    ExtendedSearchParams,
+    GravsearchGenerationService,
+    OntologyCacheService,
+    OntologyInformation,
+    ReadResource,
+    ReadResourcesSequence,
+    SearchParamsService,
+    SearchService
+} from '@knora/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'kui-list-view',
-  templateUrl: './list-view.component.html',
-  styleUrls: ['./list-view.component.scss']
+    selector: 'kui-list-view',
+    templateUrl: './list-view.component.html',
+    styleUrls: ['./list-view.component.scss']
 })
 export class ListViewComponent implements OnInit {
 
-  constructor() { }
+    offset: number = 0;
+    gravsearchGenerator: ExtendedSearchParams;
 
-  ngOnInit() {
-  }
+    result: ReadResource[] = [];
+    ontologyInfo: OntologyInformation;
+    rerender: boolean = false;
 
+    searchQuery: string;
+    searchMode: string;
+
+    errorMessage: any = undefined;
+    navigationSubscription: Subscription;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _searchService: SearchService,
+        private _cacheService: OntologyCacheService,
+        private _searchParamsService: SearchParamsService,
+        private _gravSearchService: GravsearchGenerationService,
+        private _router: Router
+    ) {
+    }
+
+    ngOnInit() {
+        this.navigationSubscription = this._route.paramMap.subscribe((params: Params) => {
+            this.searchMode = params.get('mode');
+
+            // init offset to 0
+            this.offset = 0;
+            this.result = [];
+
+            if (this.searchMode === 'fulltext') {
+                this.searchQuery = params.get('q');
+            } else if (this.searchMode === 'extended') {
+                this.gravsearchGenerator = this._searchParamsService.getSearchParams();
+                this.generateGravsearchQuery();
+            }
+
+            this.rerender = true;
+            this.getResult();
+        });
+    }
+
+    /**
+       * Get search result from Knora - 2 cases: simple search and extended search
+       */
+    getResult() {
+        // this.isLoading = true;
+
+        // FULLTEXT SEARCH
+        if (this.searchMode === 'fulltext') {
+            // perform count query
+            /* if (this.offset === 0) {
+
+                this._searchService.doFullTextSearchCountQueryCountQueryResult(this.searchQuery)
+                    .subscribe(
+                        this.showNumberOfAllResults,
+                        (error: any) => {
+                            this.errorMessage = <any>error;
+                            // console.log('numberOfAllResults', this.numberOfAllResults);
+                        }
+                    );
+            } */
+
+            // perform full text search
+            console.log('fulltext', this.searchMode);
+            this._searchService.doFullTextSearchReadResourceSequence(this.searchQuery, this.offset)
+                .subscribe(
+                    this.processSearchResults, // function pointer
+                    (error: any) => {
+                        this.errorMessage = <any>error;
+                    },
+            );
+
+            // EXTENDED SEARCH
+        } else if (this.searchMode === 'extended') {
+            // perform count query
+            /* if (this.offset === 0) {
+                this._searchService.doExtendedSearchCountQueryCountQueryResult(this.searchQuery)
+                    .subscribe(
+                        this.showNumberOfAllResults,
+                        (error: any) => {
+                            this.errorMessage = <any>error;
+                        }
+                    );
+            } */
+            console.log('extended', this.searchMode);
+            this._searchService.doExtendedSearchReadResourceSequence(this.searchQuery)
+                .subscribe(
+                    this.processSearchResults, // function pointer
+                    (error: any) => {
+                        this.errorMessage = <any>error;
+                    });
+
+        } else {
+            this.errorMessage = `search mode invalid: ${this.searchMode}`;
+        }
+    }
+
+    /**
+     * Generates the Gravsearch query for the current offset.
+     */
+    private generateGravsearchQuery() {
+
+        const gravsearch: string | boolean = this.gravsearchGenerator.generateGravsearch(this.offset);
+        if (gravsearch === false) {
+            // no valid search params (application has been reloaded)
+            // go to root
+            this._router.navigate([''], { relativeTo: this._route });
+            return;
+        } else {
+            this.searchQuery = <string>gravsearch;
+        }
+    }
+
+    private processSearchResults = (searchResult: ReadResourcesSequence) => {
+
+        console.log('process search results');
+        // assign ontology information to a variable so it can be used in the component's template
+        if (this.ontologyInfo === undefined) {
+            // init ontology information
+            this.ontologyInfo = searchResult.ontologyInformation;
+        } else {
+            // update ontology information
+            this.ontologyInfo.updateOntologyInformation(searchResult.ontologyInformation);
+        }
+        // append results to search results
+        // console.log('results 1', this.result);
+        this.result = this.result.concat(searchResult.resources);
+
+        // this.isLoading = false;
+        this.rerender = false;
+
+    }
 }
