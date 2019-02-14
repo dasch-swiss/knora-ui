e7e62462b9ec186dd0f61223f58f669f008bc115
Tobias Schweizer <t.schweizer@unibas.ch>
2019-02-14 13:51:11 +0100

    tests (core): fix test for list

--- projects/knora/core/src/lib/services/v2/list.service.spec.ts
+++ projects/knora/core/src/lib/services/v2/list.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ListService } from './list.service';
+import { KuiCoreModule } from '../../core.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ListService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [
+            HttpClientTestingModule,
+            KuiCoreModule.forRoot({name: '', api: 'http://0.0.0.0:3333', app: '', media: ''})
+        ]
+    }));
 
     it('should be created', () => {
         const service: ListService = TestBed.get(ListService);
