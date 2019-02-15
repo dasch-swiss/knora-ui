b04a4ba8f838e91705cec7fca4c6ef1abe602cd2
Ivan Subotic <ibs@subotic.ch>
2019-02-15 11:44:42 +0100

    feature (core): config token

--- projects/knora/core/src/lib/core.module.ts
+++ projects/knora/core/src/lib/core.module.ts
@@ -1,27 +1,9 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { KuiCoreConfig } from './declarations';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 export const KuiCoreConfigToken = new InjectionToken<KuiCoreConfig>('KuiCoreConfigToken (knora.core.config)');
-
-
-// Have to implement as we need to return a class from the provider, we should consider exporting
-// this in the firebase/app types as this is our highest risk of breaks
-export class KuiCore {}
-
-export function _kuiCoreFactory(config: KuiCoreConfig) {
-    console.log('KuiCoreModule - _kuiCoreFactory - config', config)
-    return ( KuiCoreModule.initializeApp(config) );
-}
-
-const KuiCoreProvider = {
-    provide: KuiCore,
-    useFactory: _kuiCoreFactory,
-    deps: [
-        KuiCoreConfigToken
-    ]
-};
 
 @NgModule({
     imports: [
@@ -31,8 +13,7 @@ const KuiCoreProvider = {
     declarations: [],
     exports: [
         HttpClientModule
-    ],
-    providers: [ KuiCoreProvider ]
+    ]
 })
 export class KuiCoreModule {
     /**
@@ -43,17 +24,6 @@ export class KuiCoreModule {
     static forRoot(config: KuiCoreConfig): ModuleWithProviders {
         // get the app environment configuration here
         console.log('KuiCoreModule - forRoot - config: ', config);
-        return {
-            ngModule: KuiCoreModule,
-            providers: [
-                {provide: KuiCoreConfigToken, useValue: config}
-            ]
-        };
-    }
-
-    static initializeApp(config: KuiCoreConfig): ModuleWithProviders {
-        // get the app environment configuration here
-        console.log('KuiCoreModule - initializeApp - config: ', config);
         return {
             ngModule: KuiCoreModule,
             providers: [
