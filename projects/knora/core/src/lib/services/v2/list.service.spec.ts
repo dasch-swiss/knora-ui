26a6e07a3e332bfaeea3b117d1061678e86fd0ae
Tobias Schweizer <t.schweizer@unibas.ch>
2019-02-15 14:16:06 +0100

    tests (core): test for list service

--- projects/knora/core/src/lib/services/v2/list.service.spec.ts
+++ projects/knora/core/src/lib/services/v2/list.service.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ListService } from './list.service';
+import { ListNodeV2, ListService } from './list.service';
 import { KuiCoreModule } from '../../core.module';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from '../api.service';
 
-fdescribe('ListService', () => {
+describe('ListService', () => {
     let httpTestingController: HttpTestingController;
     let listService: ListService;
 
@@ -41,11 +41,30 @@ fdescribe('ListService', () => {
         const expectedList = require('../../test-data/list/treeList.json');
 
         listService.getList('http://rdfh.ch/lists/0001/treeList').subscribe(
-            (list) => {
-                console.log(list);
+            (list: ListNodeV2) => {
+
+                expect(list.id).toEqual('http://rdfh.ch/lists/0001/treeList');
+                expect(list.label).toEqual('Tree list root');
+                expect(list.children.length).toEqual(3);
+
+                expect(list.children[2].id).toEqual('http://rdfh.ch/lists/0001/treeList03');
+                expect(list.children[2].children.length).toEqual(2);
+
+                expect(list.children[2].children[0].id).toEqual('http://rdfh.ch/lists/0001/treeList10');
+                expect(list.children[2].children[1].id).toEqual('http://rdfh.ch/lists/0001/treeList11');
+
+                // do the same request again (should read from cache)
+                // NOTE: the requests have to be chained and cannot be executed concurrently
+                listService.getList('http://rdfh.ch/lists/0001/treeList').subscribe(
+                    (list2) => {
+                        expect(list2.id).toEqual('http://rdfh.ch/lists/0001/treeList');
+                        expect(list2.label).toEqual('Tree list root');
+                    }
+                );
             }
         );
 
+        // only one request to Knora is expected
         const httpRequest = httpTestingController.expectOne('http://0.0.0.0:3333/v2/lists/' + encodeURIComponent('http://rdfh.ch/lists/0001/treeList'));
 
         expect(httpRequest.request.method).toEqual('GET');
