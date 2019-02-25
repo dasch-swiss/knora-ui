c0be873dfedb86f6f33cf89cac8328580b1d4de3
Ivan Subotic <ibs@subotic.ch>
2019-02-25 11:07:55 +0100

    doc: rebuild documentation

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -5651,7 +5651,7 @@ window.global = window;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/ak/Code/knora-ui/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/subotic/_github.com/dhlab-basel/Knora-ui/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
