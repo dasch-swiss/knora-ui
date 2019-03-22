1fae24cbe334f1e4c0e3c036467047497ec1830c
André Kilchenmann <github@milchkannen.ch>
2019-03-22 15:36:35 +0100

    test (search): fix tests

--- projects/knora/search/src/lib/search-panel/search-panel.component.spec.ts
+++ projects/knora/search/src/lib/search-panel/search-panel.component.spec.ts
@@ -1,18 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {
-  MatAutocompleteModule,
-  MatCheckboxModule,
-  MatDatepickerModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatListModule,
-  MatSelectModule
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSelectModule
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JdnDatepickerDirective, ReversePipe } from '@knora/action';
+import { KuiCoreConfig, KuiCoreConfigToken } from '@knora/core';
 import { SearchPanelComponent } from './search-panel.component';
 import { FulltextSearchComponent } from '../fulltext-search/fulltext-search.component';
 import { ExtendedSearchComponent } from '../extended-search/extended-search.component';
@@ -27,63 +29,72 @@ import { IntegerValueComponent } from '../extended-search/select-property/specif
 import { LinkValueComponent } from '../extended-search/select-property/specify-property-value/link-value/link-value.component';
 import { TextValueComponent } from '../extended-search/select-property/specify-property-value/text-value/text-value.component';
 import { UriValueComponent } from '../extended-search/select-property/specify-property-value/uri-value/uri-value.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SearchPanelComponent', () => {
-  let component: SearchPanelComponent;
-  let fixture: ComponentFixture<SearchPanelComponent>;
+    let component: SearchPanelComponent;
+    let fixture: ComponentFixture<SearchPanelComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        MatAutocompleteModule,
-        MatCheckboxModule,
-        MatDatepickerModule,
-        MatIconModule,
-        MatListModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        FormsModule,
-        ReactiveFormsModule,
-        RouterTestingModule,
-        BrowserAnimationsModule
-      ],
-      declarations: [
-        SearchPanelComponent,
-        FulltextSearchComponent,
-        ExtendedSearchComponent,
-        SelectOntologyComponent,
-        SelectPropertyComponent,
-        SelectResourceClassComponent,
-        SpecifyPropertyValueComponent,
-        BooleanValueComponent,
-        DateValueComponent,
-        DecimalValueComponent,
-        IntegerValueComponent,
-        LinkValueComponent,
-        TextValueComponent,
-        UriValueComponent,
-        ReversePipe,
-        JdnDatepickerDirective
-      ],
-      providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            params: null
-          },
-        }
-      ]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MatAutocompleteModule,
+                MatCheckboxModule,
+                MatDatepickerModule,
+                MatIconModule,
+                MatListModule,
+                MatMenuModule,
+                MatFormFieldModule,
+                MatSelectModule,
+                FormsModule,
+                ReactiveFormsModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                HttpClientTestingModule,
+                HttpClientModule,
+            ],
+            declarations: [
+                SearchPanelComponent,
+                FulltextSearchComponent,
+                ExtendedSearchComponent,
+                SelectOntologyComponent,
+                SelectPropertyComponent,
+                SelectResourceClassComponent,
+                SpecifyPropertyValueComponent,
+                BooleanValueComponent,
+                DateValueComponent,
+                DecimalValueComponent,
+                IntegerValueComponent,
+                LinkValueComponent,
+                TextValueComponent,
+                UriValueComponent,
+                ReversePipe,
+                JdnDatepickerDirective
+            ],
+            providers: [
+                {
+                    provide: KuiCoreConfigToken,
+                    useValue: KuiCoreConfig
+                },
+                HttpClient,
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        params: null
+                    }
+                }
+            ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchPanelComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchPanelComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
