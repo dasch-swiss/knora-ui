879dff173ca3f9c46b556a5aeb2361291868aed4
Tobias Schweizer <t.schweizer@unibas.ch>
2019-05-09 11:08:06 +0200

    test (list display comp): add basic test

--- projects/knora/search/src/lib/extended-search/select-property/specify-property-value/list-value/list-display/list-display.component.spec.ts
+++ projects/knora/search/src/lib/extended-search/select-property/specify-property-value/list-value/list-display/list-display.component.spec.ts
@@ -1,25 +1,128 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListDisplayComponent } from './list-display.component';
+import { Component, DebugElement, OnInit, ViewChild } from '@angular/core';
+import { ListNodeV2 } from '@knora/core';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-xdescribe('ListDisplayComponent', () => {
-  let component: ListDisplayComponent;
-  let fixture: ComponentFixture<ListDisplayComponent>;
+describe('ListDisplayComponent', () => {
+    let testHostComponent: TestHostComponent;
+    let testHostFixture: ComponentFixture<TestHostComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ListDisplayComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ListDisplayComponent,
+                TestHostComponent
+            ],
+            imports: [
+                MatMenuModule,
+                BrowserAnimationsModule
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ListDisplayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        testHostFixture = TestBed.createComponent(TestHostComponent);
+        testHostComponent = testHostFixture.componentInstance;
+        testHostFixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        expect(testHostComponent).toBeTruthy();
+    });
+
+    it('should create', () => {
+        // access the test host component's child
+        expect(testHostComponent.listDisplay).toBeTruthy();
+    });
+
+    it('should open the menu for child nodes', () => {
+
+        const ele: DebugElement = testHostFixture.debugElement;
+
+        ele.nativeElement.click();
+
+        testHostFixture.detectChanges();
+
+        const buttonDe = ele.query(By.css('button'));
+
+        const buttonEle: HTMLElement = buttonDe.nativeElement;
+
+        buttonEle.click();
+
+        testHostFixture.detectChanges();
+
+    });
 });
+
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    selector: `host-component`,
+    template: `
+        <button mat-stroked-button [matMenuTriggerFor]="mainMenu" type="button">
+            <span *ngIf="!selectedNode">Select list value</span>
+            <span *ngIf="selectedNode">{{selectedNode.label}}</span>
+        </button>
+        
+        <mat-menu #mainMenu="matMenu" [overlapTrigger]="false">
+        <button mat-menu-item [matMenuTriggerFor]="listDispl.childMenu" (click)="getSelectedNode(testList)"
+                type="button">
+            {{testList.label}}
+        </button>
+        <list-display #listDispl [children]="testList.children" (selectedNode)="getSelectedNode($event)">
+        </list-display>
+    </mat-menu>`
+})
+class TestHostComponent implements OnInit {
+
+    testList;
+
+    selectedNode: ListNodeV2;
+
+    @ViewChild('listDispl') listDisplay: ListDisplayComponent;
+
+    @ViewChild(MatMenuTrigger) menuTrigger: MatMenuTrigger;
+
+    constructor() {
+    }
+
+    getSelectedNode(item: ListNodeV2) {
+        // this.menuTrigger.closeMenu();
+        this.selectedNode = item;
+
+        // this.form.controls['listValue'].setValue(item.id);
+    }
+
+    ngOnInit() {
+
+        const testList = new ListNodeV2(
+            'http://rdfh.ch/lists/0001/treeList',
+            'tree list'
+        );
+
+        const testListChild1 = new ListNodeV2(
+            'http://rdfh.ch/lists/0001/treeList/01',
+            'tree list 01',
+            1,
+            'http://rdfh.ch/lists/0001/treeList'
+        );
+
+        const testListChild2 = new ListNodeV2(
+            'http://rdfh.ch/lists/0001/treeList/02',
+            'tree list 02',
+            2,
+            'http://rdfh.ch/lists/0001/treeList'
+        );
+
+        testListChild1.children.push(testListChild2);
+
+        testList.children.push(testListChild1);
+
+        this.testList = testList;
+    }
+}
--- projects/knora/search/src/lib/extended-search/select-property/specify-property-value/list-value/list-display/list-display.component.ts
+++ projects/knora/search/src/lib/extended-search/select-property/specify-property-value/list-value/list-display/list-display.component.ts
@@ -1,86 +1,29 @@
-import { Component, Inject, Input, OnChanges, OnDestroy, OnInit, ViewChild, Output, EventEmitter, QueryList } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { ListNodeV2 } from '@knora/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatMenuTrigger, MatMenu } from '@angular/material';
-
-// https://stackoverflow.com/questions/45661010/dynamic-nested-reactive-form-expressionchangedafterithasbeencheckederror
-const resolvedPromise = Promise.resolve(null);
+import { MatMenuTrigger } from '@angular/material';
 
 @Component({
     selector: 'list-display',
     templateUrl: './list-display.component.html',
     styleUrls: ['./list-display.component.scss']
 })
-export class ListDisplayComponent implements OnInit, OnDestroy {
+export class ListDisplayComponent implements OnInit {
 
-    @Input() children?: ListNodeV2;
+    @Input() children: ListNodeV2[];
 
     @Output() selectedNode: EventEmitter<ListNodeV2> = new EventEmitter<ListNodeV2>();
 
-    // parent FormGroup
-    // @Input() formGroup?: FormGroup;
-
     @ViewChild('childMenu') public childMenu: MatMenuTrigger;
-    /*
 
-        @ViewChild('matTrigger') public matTrigger: MatMenuTrigger; */
-
-    // private _listNode: ListNodeV2;
-
-    /*     @Input()
-        set listNode(value: ListNodeV2) {
-            console.log(value)
-            this.activeNode = undefined;
-            this._listNode = value;
-        }
-
-        get listNode() {
-            return this._listNode;
-        } */
-
-    // form: FormGroup;
-
-    // activeNode;
-    // @Inject(FormBuilder) private fb: FormBuilder
     constructor () {
     }
 
     ngOnInit() {
-        /* this.form = this.fb.group({
-            listValue: [null, Validators.compose([Validators.required])]
-        });
-
-        this.form.valueChanges.subscribe(
-            (data) => {
-                this.activeNode = data.listValue;
-            }
-        );
-
-        resolvedPromise.then(() => {
-            // add form to the parent form group
-            this.formGroup.addControl('propValue', this.form);
-        }); */
-    }
-
-    ngOnDestroy() {
-
-        // remove form from the parent form group
-        /*  resolvedPromise.then(() => {
-             this.formGroup.removeControl('propValue');
-         }); */
-
+        // console.log(this.children)
     }
 
     setValue(item: ListNodeV2) {
         this.selectedNode.emit(item);
     }
-    /*
-        menuClosed(ev: any) {
-            console.log('menu closed', ev);
-            if (ev) {
-                this.matTrigger.closeMenu();
-            }
-
-        } */
 
 }
