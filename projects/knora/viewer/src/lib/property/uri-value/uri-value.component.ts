abe94b4693bafb9e4f60bcfb33859c1fd20f093b
Sepideh Alassi <sepideh.alassi@gmail.com>
2019-05-27 14:50:26 +0200

    feature (uriLabel) display the label of a URI property if given

--- projects/knora/viewer/src/lib/property/uri-value/uri-value.component.ts
+++ projects/knora/viewer/src/lib/property/uri-value/uri-value.component.ts
@@ -1,24 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ReadUriValue } from '@knora/core';
+import { isUndefined } from 'util';
 
 @Component({
-  selector: 'kui-uri-value',
+  selector: '   kui-uri-value',
   templateUrl: './uri-value.component.html',
   styleUrls: ['./uri-value.component.scss']
 })
-export class UriValueComponent {
+export class UriValueComponent implements OnChanges {
 
   @Input()
   set valueObject(value: ReadUriValue) {
     this.__uriValueObj = value;
-  }
 
+  }
   get valueObject() {
     return this.__uriValueObj;
   }
-
+  @Input() label?: string;
   private __uriValueObj: ReadUriValue;
-
+  public displayString: string;
   constructor() { }
+
+  ngOnChanges() {
+      if (this.label === undefined) {
+          this.displayString = this.__uriValueObj.uri;
+      } else {
+          this.displayString = this.label;
+      }
+  }
 
 }
