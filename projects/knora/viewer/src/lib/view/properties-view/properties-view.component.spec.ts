f6eaaee4c61c63b8bc8e62f7bd07e873da28ab8e
André Kilchenmann <github@milchkannen.ch>
2019-06-15 08:26:04 +0200

    test(viewer): Fix tests

--- projects/knora/viewer/src/lib/view/properties-view/properties-view.component.spec.ts
+++ projects/knora/viewer/src/lib/view/properties-view/properties-view.component.spec.ts
@@ -1,9 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PropertiesViewComponent } from './properties-view.component';
-import { MatCardModule } from '@angular/material';
+import { MatCardModule, MatListModule, MatTabsModule, MatCheckboxModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { KuiCoreModule } from '@knora/core';
+
+import { BooleanValueComponent } from '../../property/boolean-value/boolean-value.component';
+import { ColorValueComponent } from '../../property/color-value/color-value.component';
+import { DateValueComponent } from '../../property/date-value/date-value.component';
+import { DecimalValueComponent } from '../../property/decimal-value/decimal-value.component';
+import { GeometryValueComponent } from '../../property/geometry-value/geometry-value.component';
+import { IntegerValueComponent } from '../../property/integer-value/integer-value.component';
+import { IntervalValueComponent } from '../../property/interval-value/interval-value.component';
+import { LinkValueComponent } from '../../property/link-value/link-value.component';
+import { ListValueComponent } from '../../property/list-value/list-value.component';
+import { TextValueAsStringComponent } from '../../property/text-value/text-value-as-string/text-value-as-string.component';
+import { TextValueAsHtmlComponent } from '../../property/text-value/text-value-as-html/text-value-as-html.component';
+import { TextValueAsXmlComponent } from '../../property/text-value/text-value-as-xml/text-value-as-xml.component';
+import { TextfileValueComponent } from '../../property/textfile-value/textfile-value.component';
+import { UriValueComponent } from '../../property/uri-value/uri-value.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('PropertiesViewComponent', () => {
     let component: PropertiesViewComponent;
@@ -12,11 +29,32 @@ describe('PropertiesViewComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
+                BrowserAnimationsModule,
                 HttpClientModule,
+                MatCheckboxModule,
                 KuiCoreModule,
-                MatCardModule
+                MatCardModule,
+                MatListModule,
+                MatTabsModule,
+                RouterTestingModule
             ],
-            declarations: [PropertiesViewComponent]
+            declarations: [
+                PropertiesViewComponent,
+                BooleanValueComponent,
+                ColorValueComponent,
+                DateValueComponent,
+                DecimalValueComponent,
+                GeometryValueComponent,
+                IntegerValueComponent,
+                IntervalValueComponent,
+                LinkValueComponent,
+                ListValueComponent,
+                TextValueAsStringComponent,
+                TextValueAsHtmlComponent,
+                TextValueAsXmlComponent,
+                TextfileValueComponent,
+                UriValueComponent
+            ]
         })
             .compileComponents();
     }));
