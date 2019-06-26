a7d69b80f815d95f7684c9237dd7bdb2fb9dfd6a
André Kilchenmann <github@milchkannen.ch>
2019-06-26 19:09:31 +0200

    style(search): Fix size issue in search log list (#258)
    * style(search): Fix size issue in search log list
    
    * feat(search): Optimized filter by project
    
    * test(search): Fix broken tests
    
    * style(search): Fix margin issue

--- projects/knora/search/src/lib/fulltext-search/fulltext-search.component.spec.ts
+++ projects/knora/search/src/lib/fulltext-search/fulltext-search.component.spec.ts
@@ -1,19 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule
-} from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { ReversePipe } from '@knora/action';
-import { FulltextSearchComponent } from './fulltext-search.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatTooltipModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { KuiActionModule } from '@knora/action';
 import { KuiCoreConfig, KuiCoreConfigToken } from '@knora/core';
+import { FulltextSearchComponent } from './fulltext-search.component';
 
 describe('FulltextSearchComponent', () => {
     let component: FulltextSearchComponent;
@@ -24,15 +19,17 @@ describe('FulltextSearchComponent', () => {
             imports: [
                 HttpClientTestingModule,
                 HttpClientModule,
+                KuiActionModule,
                 MatIconModule,
                 MatInputModule,
                 MatListModule,
                 MatMenuModule,
+                MatTooltipModule,
                 FormsModule,
                 RouterTestingModule,
                 BrowserAnimationsModule
             ],
-            declarations: [FulltextSearchComponent, ReversePipe],
+            declarations: [FulltextSearchComponent],
             providers: [
                 {
                     provide: KuiCoreConfigToken,
