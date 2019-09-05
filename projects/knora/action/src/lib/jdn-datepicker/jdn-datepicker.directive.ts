1dc1c1c8f96c135483cece26525e1a89637851a3
André Kilchenmann <github@milchkannen.ch>
2019-09-05 08:29:50 +0200

    fix(action): Fix IDE issue about selector; IDE changed the indent

--- projects/knora/action/src/lib/jdn-datepicker/jdn-datepicker.directive.ts
+++ projects/knora/action/src/lib/jdn-datepicker/jdn-datepicker.directive.ts
@@ -7,11 +7,9 @@ import { JDNConvertibleCalendar } from 'jdnconvertiblecalendar';
 * JdnDatepickerDirective creates a wrapper element that provides a new adapter with each instance of the datepicker.
 */
 @Directive({
-    selector: 'kuiJdnDatepicker',
-    providers: [
-        { provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter, deps: [MAT_DATE_LOCALE] }
-    ]
+    selector: '[kuiJdnDatepicker]',
+    providers: [{ provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter, deps: [MAT_DATE_LOCALE] }]
 })
 export class JdnDatepickerDirective {
-    constructor(private adapter: DateAdapter<JDNConvertibleCalendar>) { }
+    constructor (private adapter: DateAdapter<JDNConvertibleCalendar>) { }
 }
