ebd96815d1fed07f9da077f58438cab4fa6de625
André Kilchenmann <github@milchkannen.ch>
2019-09-06 10:46:43 +0200

    test(action): Fix the broken tests

--- projects/knora/action/src/lib/string-literal-input/string-literal-input.component.spec.ts
+++ projects/knora/action/src/lib/string-literal-input/string-literal-input.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StringLiteralInputComponent } from './string-literal-input.component';
 
 describe('StringLiteralInputComponent', () => {
-  let component: StringLiteralInputComponent;
-  let fixture: ComponentFixture<StringLiteralInputComponent>;
+    let component: StringLiteralInputComponent;
+    let fixture: ComponentFixture<StringLiteralInputComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ StringLiteralInputComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MatMenuModule,
+                MatInputModule,
+                MatIconModule,
+                MatButtonToggleModule,
+                MatFormFieldModule,
+                BrowserAnimationsModule,
+                ReactiveFormsModule
+            ],
+            declarations: [
+                StringLiteralInputComponent
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StringLiteralInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(StringLiteralInputComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
