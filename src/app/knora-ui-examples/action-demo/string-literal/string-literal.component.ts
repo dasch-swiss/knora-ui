317d220578a5cad229b440a375999ae7ed8873f4
André Kilchenmann <github@milchkannen.ch>
2019-09-15 14:22:29 +0200

    docs(demo): Update stringifyStringLiteral demo

--- src/app/knora-ui-examples/action-demo/string-literal/string-literal.component.ts
+++ src/app/knora-ui-examples/action-demo/string-literal/string-literal.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { AppDemo } from 'src/app/app.config';
+import { Example } from 'src/app/app.interfaces';
+import { StringLiteral } from '@knora/core';
 
 @Component({
-  selector: 'app-string-literal',
-  templateUrl: './string-literal.component.html',
-  styleUrls: ['./string-literal.component.scss']
+    selector: 'app-string-literal',
+    templateUrl: './string-literal.component.html',
+    styleUrls: ['./string-literal.component.scss']
 })
 export class StringLiteralComponent implements OnInit {
 
-  constructor() { }
+    module = AppDemo.actionModule;
 
-  ngOnInit() {
-  }
+    labels: StringLiteral[] = [
+        {
+            value: 'Welt',
+            language: 'de'
+        },
+        {
+            value: 'World',
+            language: 'en'
+        },
+        {
+            value: 'Monde',
+            language: 'fr'
+        },
+        {
+            value: 'Mondo',
+            language: 'it'
+        },
+    ];
+
+    // demo configuration incl. code to display
+    stringifyPipe: Example = {
+        title: 'StringifyStringLiteral Pipe',
+        subtitle: '',
+        name: 'stringifyPipe',
+        code: {
+            html: `
+<strong>Show all values</strong>
+<p>{{labels | kuiStringifyStringLiteral:'all'}}</p>
+
+<strong>Show only one value</strong>
+<p>{{labels | kuiStringifyStringLiteral}}</p>
+`,
+            ts: `
+labels: StringLiteral[] = '` + JSON.stringify(this.labels) + `';
+`,
+            scss: ''
+        }
+    };
+
+    constructor () { }
+
+    ngOnInit() {
+    }
 
 }
