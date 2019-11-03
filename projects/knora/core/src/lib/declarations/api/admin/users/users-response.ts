12a1bfa7576bce2e4f3c12395295a2e085a19c66
André Kilchenmann <github@milchkannen.ch>
2019-11-03 10:51:26 +0100

    docs(core): Start to add @deprecated to the jsDocs

--- projects/knora/core/src/lib/declarations/api/admin/users/users-response.ts
+++ projects/knora/core/src/lib/declarations/api/admin/users/users-response.ts
@@ -1,6 +1,9 @@
 import { JsonObject, JsonProperty } from 'json2typescript';
 import { User } from './user';
 
+/**
+ * @deprecated Use UsersResponse from @knora/api (github:dasch-swiss/knora-api-js-lib) instead
+ */
 @JsonObject('UsersResponse')
 export class UsersResponse {
 
