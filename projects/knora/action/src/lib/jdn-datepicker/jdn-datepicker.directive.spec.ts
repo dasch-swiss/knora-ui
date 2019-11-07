c3b880f35080195a153eb4425e36aacca1ebbf87
Flavie Laurens <flavilo@hotmail.fr>
2019-11-07 17:50:52 +0100

    fix(action): Create test host component to mock dateValueComponent

--- projects/knora/action/src/lib/jdn-datepicker/jdn-datepicker.directive.spec.ts
+++ projects/knora/action/src/lib/jdn-datepicker/jdn-datepicker.directive.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DebugElement } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DebugElement, Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { DateAdapter, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -12,27 +12,61 @@ import { JDNConvertibleCalendar } from 'jdnconvertiblecalendar';
 import { DateValueComponent } from '@knora/search';
 
 describe('JdnDatepickerDirective', () => {
-    let component: DateValueComponent;
-    let fixture: ComponentFixture<DateValueComponent>;
     let jdnDatepicker: DebugElement;
     let adapter: DateAdapter<JDNConvertibleCalendar>;
 
+    let testHostComponent: TestHostComponent;
+    let testHostFixture: ComponentFixture<TestHostComponent>;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, FormsModule, ReactiveFormsModule],
-            declarations: [DateValueComponent, JdnDatepickerDirective],
+            imports: [
+                MatDatepickerModule,
+                MatFormFieldModule,
+                MatNativeDateModule,
+                FormsModule,
+                ReactiveFormsModule
+            ],
+            declarations: [
+                DateValueComponent,
+                TestHostComponent,
+                JdnDatepickerDirective
+            ],
             providers: [
-                { provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter, deps: [MAT_DATE_LOCALE] }
+                { provide: DateAdapter, useClass: JDNConvertibleCalendarDateAdapter, deps: [MAT_DATE_LOCALE] },
+                FormBuilder
             ]
         });
-        fixture = TestBed.createComponent(DateValueComponent);
-        component = fixture.componentInstance;
-        jdnDatepicker = fixture.debugElement.query(By.css('jdnDatepicker'));
+        testHostFixture = TestBed.createComponent(TestHostComponent);
+        testHostComponent = testHostFixture.componentInstance;
+        jdnDatepicker = testHostFixture.debugElement.query(By.css('jdnDatepicker'));
     });
 
-    xit('should create an instance', () => {
+    it('should create an instance', () => {
         const directive = new JdnDatepickerDirective(adapter);
         expect(directive).toBeTruthy();
     });
 
 });
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    selector: `host-component`,
+    template: `
+        <date-value #dateVal [formGroup]="form"></date-value>`
+})
+class TestHostComponent implements OnInit {
+
+    form;
+
+    @ViewChild('dateVal', { static: false }) dateValue: DateValueComponent;
+
+    constructor(@Inject(FormBuilder) private fb: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.form = this.fb.group({});
+    }
+}
