45d5ae329e036c44ce509be121f7dd7e9d05d5d8
André Kilchenmann <github@milchkannen.ch>
2019-11-19 17:26:12 +0100

    test: Add test for session service

--- projects/knora/core/src/lib/session.service.spec.ts
+++ projects/knora/core/src/lib/session.service.spec.ts
@@ -1,12 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { KnoraApiConfig, KnoraApiConnection } from '@knora/api';
 
+import { KnoraApiConfigToken, KnoraApiConnectionToken, KuiCoreModule } from './core.module';
 import { SessionService } from './session.service';
 
-describe('SessionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('SessionService', () => {
+    let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
+    let sessionService: SessionService;
 
-  it('should be created', () => {
-    const service: SessionService = TestBed.get(SessionService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                KuiCoreModule.forRoot({
+                    knora: {
+                        apiProtocol: 'http',
+                        apiHost: '0.0.0.0',
+                        apiPort: 3333,
+                        apiUrl: '',
+                        apiPath: '',
+                        jsonWebToken: '',
+                        logErrors: true
+                    },
+                    app: {
+                        name: 'Knora-UI-APP',
+                        url: 'localhost:4200'
+                    }
+                })
+            ],
+            providers: [
+                SessionService,
+                {
+                    provide: KnoraApiConfigToken,
+                    useValue: KnoraApiConfig
+                },
+                {
+                    provide: KnoraApiConnectionToken,
+                    useValue: KnoraApiConnection
+                }
+            ]
+        });
+
+
+        // Inject the http, test controller, and service-under-test
+        // as they will be referenced by each test.
+        httpClient = TestBed.get(HttpClient);
+        httpTestingController = TestBed.get(HttpTestingController);
+        sessionService = TestBed.get(SessionService);
+    });
+
+    it('should be created', inject([SessionService], (service: SessionService) => {
+        expect(service).toBeTruthy();
+    }));
 });
