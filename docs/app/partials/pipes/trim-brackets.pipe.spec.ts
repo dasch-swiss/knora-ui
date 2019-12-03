c14920a982a96c33307d246f564820c0b667f6ef
André Kilchenmann <github@milchkannen.ch>
2019-12-03 13:33:51 +0100

    docs: Update documentation

--- docs/app/partials/pipes/trim-brackets.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TrimBracketsPipe } from './trim-brackets.pipe';
-
-describe('TrimBracketsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TrimBracketsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
