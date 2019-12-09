2d9fbeb34312d40e5f5b73462d4554f4275447b2
André Kilchenmann <github@milchkannen.ch>
2019-12-09 11:46:19 +0100

    teest(action): Fix tests

--- projects/knora/action/src/lib/string-literal-input/string-literal-input.component.spec.ts
+++ projects/knora/action/src/lib/string-literal-input/string-literal-input.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { MatButtonToggleModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StringLiteralInputComponent } from './string-literal-input.component';
@@ -7,6 +7,8 @@ import { StringLiteralInputComponent } from './string-literal-input.component';
 describe('StringLiteralInputComponent', () => {
     let component: StringLiteralInputComponent;
     let fixture: ComponentFixture<StringLiteralInputComponent>;
+
+    const formBuilder: FormBuilder = new FormBuilder();
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -21,6 +23,12 @@ describe('StringLiteralInputComponent', () => {
             ],
             declarations: [
                 StringLiteralInputComponent
+            ],
+            providers: [
+                {
+                    provide: FormBuilder,
+                    useValue: formBuilder
+                }
             ]
         })
             .compileComponents();
@@ -29,6 +37,9 @@ describe('StringLiteralInputComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(StringLiteralInputComponent);
         component = fixture.componentInstance;
+        component.form = formBuilder.group({
+            text: null
+        });
         fixture.detectChanges();
     });
 
