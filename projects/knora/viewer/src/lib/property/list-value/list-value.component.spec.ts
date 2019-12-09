100fb2419314e1ed0dcdb74635217c54c4bbc077
Flavie Laurens <flavilo@hotmail.fr>
2019-12-09 18:33:33 +0100

    test(viewer): Fix list-value tests + remove unnecessary node observable

--- projects/knora/viewer/src/lib/property/list-value/list-value.component.spec.ts
+++ projects/knora/viewer/src/lib/property/list-value/list-value.component.spec.ts
@@ -1,25 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ListValueComponent } from './list-value.component';
 import { Component, DebugElement, OnInit, ViewChild } from '@angular/core';
-import { KuiCoreConfig, KuiConfigToken, ListCacheService, ReadListValue } from '@knora/core';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { of } from 'rxjs';
-import { ListNodeV2 } from '@knora/core';
+import { ReadListValue } from '@knora/api';
+
+import { ListValueComponent } from './list-value.component';
+
+/**
+ * Test host component to simulate parent component.
+ */
+@Component({
+    template: `<kui-list-value #listVal [valueObject]="listValue"></kui-list-value>`
+})
+class TestHostComponent implements OnInit {
+
+    @ViewChild('listVal', { static: false }) listValueComponent: ListValueComponent;
+
+    listValue: ReadListValue;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.listValue = new ReadListValue();
+        this.listValue.listNode = 'http://rdfh.ch/8be1b7cf7103';
+        this.listValue.listNodeLabel = 'countries';
+    }
+}
 
 describe('ListValueComponent', () => {
     let testHostComponent: TestHostComponent;
     let testHostFixture: ComponentFixture<TestHostComponent>;
 
-    let spyListCacheService;
-
     beforeEach(async(() => {
-
-        spyListCacheService = jasmine.createSpyObj('ListCacheService', ['getListNode']);
 
         TestBed.configureTestingModule({
             declarations: [
@@ -32,24 +48,16 @@ describe('ListValueComponent', () => {
                 RouterTestingModule.withRoutes([]),
             ],
             providers: [
-                { provide: ListCacheService, useValue: spyListCacheService },
                 {
                     provide: ActivatedRoute,
                     useValue: {
                         params: null
                     },
-                },
-                {
-                    provide: KuiConfigToken,
-                    useValue: KuiCoreConfig
-                },
+                }
             ]
         })
             .compileComponents();
 
-        spyListCacheService.getListNode.and.callFake((nodeIri) => {
-            return of(new ListNodeV2(nodeIri, 'test' + nodeIri, 1, ''));
-        });
     }));
 
     beforeEach(() => {
@@ -65,29 +73,29 @@ describe('ListValueComponent', () => {
     });
 
     it('should be equal to the list node Iri "http://rdfh.ch/8be1b7cf7103"', () => {
-        const listCacheService = TestBed.get(ListCacheService);
-
-        expect(testHostComponent.listValueComponent.valueObject.listNodeIri).toEqual('http://rdfh.ch/8be1b7cf7103');
+        console.log('list value component ', testHostComponent.listValueComponent.valueObject);
+        expect(testHostComponent.listValueComponent.valueObject.listNode).toEqual('http://rdfh.ch/8be1b7cf7103');
 
         const hostCompDe = testHostFixture.debugElement;
 
         const listVal = hostCompDe.query(By.directive(ListValueComponent));
+        console.log('listVal', listVal);
 
         const spanDebugElement: DebugElement = listVal.query(By.css('span'));
+        console.log('spanDebugElement', spanDebugElement);
 
         const spanNativeElement: HTMLElement = spanDebugElement.nativeElement;
+        console.log('spanNativeElement', spanNativeElement);
 
-        expect(spanNativeElement.innerText).toEqual('testhttp://rdfh.ch/8be1b7cf7103');
-
-        expect(listCacheService.getListNode).toHaveBeenCalledTimes(1);
-        expect(listCacheService.getListNode).toHaveBeenCalledWith('http://rdfh.ch/8be1b7cf7103');
+        expect(spanNativeElement.innerText).toEqual('countries');
 
     });
 
     it('should be equal to the list node Iri "http://rdfh.ch/9sdf8sfd2jf9"', () => {
-        const listCacheService = TestBed.get(ListCacheService);
 
-        testHostComponent.listValue = new ReadListValue('id', 'propIri', 'http://rdfh.ch/9sdf8sfd2jf9');
+        testHostComponent.listValue = new ReadListValue();
+        testHostComponent.listValue.listNode = 'http://rdfh.ch/9sdf8sfd2jf9';
+        testHostComponent.listValue.listNodeLabel = 'cantons';
 
         testHostFixture.detectChanges();
 
@@ -99,33 +107,7 @@ describe('ListValueComponent', () => {
 
         const spanNativeElement: HTMLElement = spanDebugElement.nativeElement;
 
-        expect(spanNativeElement.innerText).toEqual('testhttp://rdfh.ch/9sdf8sfd2jf9');
-
-        expect(listCacheService.getListNode).toHaveBeenCalledTimes(2);
-        expect(listCacheService.getListNode).toHaveBeenCalledWith('http://rdfh.ch/8be1b7cf7103');
-        expect(listCacheService.getListNode).toHaveBeenCalledWith('http://rdfh.ch/9sdf8sfd2jf9');
+        expect(spanNativeElement.innerText).toEqual('cantons');
     });
 
 });
-
-
-/**
- * Test host component to simulate parent component.
- */
-@Component({
-    template: `
-        <kui-list-value #listVal [valueObject]="listValue"></kui-list-value>`
-})
-class TestHostComponent implements OnInit {
-
-    @ViewChild('listVal', { static: false }) listValueComponent: ListValueComponent;
-
-    listValue;
-
-    constructor() {
-    }
-
-    ngOnInit() {
-        this.listValue = new ReadListValue('id', 'propIri', 'http://rdfh.ch/8be1b7cf7103');
-    }
-}
--- projects/knora/viewer/src/lib/property/list-value/list-value.component.ts
+++ projects/knora/viewer/src/lib/property/list-value/list-value.component.ts
@@ -1,14 +1,12 @@
-import { Component, Inject, Input, OnChanges } from '@angular/core';
-import { KnoraApiConnection, ListNode, ReadListValue } from '@knora/api';
-import { KnoraApiConnectionToken } from '@knora/core';
-import { Observable } from 'rxjs';
+import { Component, Input } from '@angular/core';
+import { ReadListValue } from '@knora/api';
 
 @Component({
     selector: 'kui-list-value',
     templateUrl: './list-value.component.html',
     styleUrls: ['./list-value.component.scss']
 })
-export class ListValueComponent implements OnChanges {
+export class ListValueComponent {
 
     @Input()
     set valueObject(value: ReadListValue) {
@@ -21,17 +19,6 @@ export class ListValueComponent implements OnChanges {
 
     private _listValueObj: ReadListValue;
 
-    node: Observable<ListNode>;
-
-    constructor (
-        @Inject(KnoraApiConnectionToken) private knoraApiConnection: KnoraApiConnection
-        ) {
-    }
-
-    ngOnChanges() {
-        // given the node's Iri, ask the list cache service
-        this.node = this.knoraApiConnection.v2.listNodeCache.getNode(this._listValueObj.listNode);
-
-    }
+    constructor() { }
 
 }
