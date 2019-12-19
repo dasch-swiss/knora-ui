f6cd4ad17aaca0e57bfa94317caad2b769f7b812
Flavie Laurens <flavilo@hotmail.fr>
2019-12-19 11:29:43 +0100

    test(action): Fix test in login-form + string-literal

--- projects/knora/action/src/lib/string-literal-input/string-literal-input.component.spec.ts
+++ projects/knora/action/src/lib/string-literal-input/string-literal-input.component.spec.ts
@@ -1,7 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { MatButtonToggleModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatIconModule } from '@angular/material';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { StringLiteralInputComponent } from './string-literal-input.component';
 
 describe('StringLiteralInputComponent', () => {
@@ -38,7 +39,7 @@ describe('StringLiteralInputComponent', () => {
         fixture = TestBed.createComponent(StringLiteralInputComponent);
         component = fixture.componentInstance;
         component.form = formBuilder.group({
-            text: ['']
+            text: ['', false]
         });
         fixture.detectChanges();
     });
